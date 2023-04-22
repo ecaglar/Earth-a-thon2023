47842b86d203ba4306b620f260027f61c1d0d74b
Lucian Aiolos <luxianaiolos@gmail.com>
2023-04-22 11:48:01 -0700

    Created Login Element

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,7 +1,40 @@
 import styles from "./index.module.css";
+import { CiMail } from 'react-icons/ci'
+import { AiOutlineLock } from 'react-icons/ai'
 
 const Login = () => {
-  return <h1 className={styles.title}>Login page</h1>;
+  return (
+    <div>
+      <form method="POST" action="" className={styles.form}>
+      <h1>FlexSuppor Logo</h1>
+        <div>
+          <p className={styles.label}>Email</p>
+          <CiMail  className={styles.inputIcon}/>
+          <input 
+            className={styles.input}
+            type="text" 
+            name="email" 
+            placeholder="Your Email"
+          />
+        </div>
+        
+        <div>
+          <p className={styles.label}>Password</p>
+          <AiOutlineLock className={styles.inputIcon}/>
+          <input 
+            className={styles.input}
+            type="password" 
+            name="password" 
+            placeholder="Your Password"
+          />
+        </div>
+
+
+        <p className={styles.forgot} >Forgot Password?</p>
+        <button className={styles.button} type="submit">Login</button>
+      </form>
+    </div>
+  )
 };
 
 export default Login;
